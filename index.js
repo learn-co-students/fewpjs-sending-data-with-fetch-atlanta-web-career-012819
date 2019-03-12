8614c377d9baf12516ffef2e0bfd1304410cc08e
wulman16 <will.ulman@gmail.com>
2019-03-12 17:10:44 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let configObj = {
+  "method": "POST",
+  "body": {
+    "firstName": "Will",
+    "registryMessage": ">:("
+  }
+}
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', configObj)
+  .then(resp => resp.json())
+  .then(json => json.message)
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', configObj)
+  .then(resp => resp.json())
+  .then(json => json.message)
+  .catch(function(error) {
+    alert("Page does not exist!")
+    return ":("
+  })
+}
