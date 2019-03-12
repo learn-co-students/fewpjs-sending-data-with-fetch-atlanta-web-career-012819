208e47e6bac70c13544709a09ec81aa5db25cc2b
spikeburton <ralphburton3@gmail.com>
2019-03-12 18:44:42 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  return fetch('http://guestbook.example.com/register', {
+    method: "POST",
+    body: {
+      "firstName": "Spike",
+      "registryMessage": "This seems like a strange idea"
+    }
+  })
+  .then(r => r.json())
+  .then(json => json["message"]);
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', {
+    method: "POST",
+    body: {
+      "firstName": "Spike",
+      "registryMessage": "This seems like a strange idea"
+    }
+  })
+  .then(r => r.json())
+  .catch(e => ":(");
+}
